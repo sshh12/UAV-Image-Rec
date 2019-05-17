733188630aa77f3ae3ca25d8386438076d97d617
Shrivu Shankar <shrivu1122@gmail.com>
2019-05-17 12:57:06 -0500

    Removed Old Training Weights

--- scripts/train-detector.sh
+++ scripts/train-detector.sh
@@ -1,1 +1,1 @@
-darknet/darknet detector train model/cfgs/yolo3detector.data model/cfgs/yolo3detector-train.cfg darknet/darknet53.conv.74
+darknet/darknet detector train model/cfgs/yolo3detector.data model/cfgs/yolo3detector-train.cfg
